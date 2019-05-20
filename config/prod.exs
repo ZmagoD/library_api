596f8c825ff038405c0d08fb0e340016e39d8bc6
zmagoD <zmago_devetak@yahoo.com>
2019-05-20 22:22:10 +0200

    setup prod env

--- config/prod.exs
+++ config/prod.exs
@@ -11,8 +11,17 @@ use Mix.Config
 # before starting your production server.
 config :library_api, LibraryApiWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: System.get_env("APP_URL"), port: 443],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
+
+
+config :library_api, LibraryApi.Repo,
+  addapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || 10),
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -68,4 +77,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which should be versioned
 # separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
